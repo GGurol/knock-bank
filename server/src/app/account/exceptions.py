88754d3c8c50e78db1c48bcec48160abf4d113bc
Guney Gurol <guneygurol@yahoo.com>
2025-09-15 01:24:49 +0300

    make dynamic client changes

--- server/src/app/account/exceptions.py
+++ server/src/app/account/exceptions.py
@@ -4,40 +4,41 @@ from fastapi import HTTPException, status
 class AccountNotFound(HTTPException):
     def __init__(self) -> None:
         """Raised when account is not found by path parameter."""
-        self.detail = 'Conta não encontrada.'
+        self.detail = 'Account not found.'
         self.status_code = status.HTTP_404_NOT_FOUND
 
 
 class AccountAlreadyExistsWithThisCPF(HTTPException):
     def __init__(self) -> None:
-        """Raised when a account already exists with a cpf."""
-        self.detail = 'Esse CPF já tem uma conta cadastrada.'
+        """Raised when an account already exists with a CPF."""
+        self.detail = 'This CPF already has a registered account.'
         self.status_code = status.HTTP_400_BAD_REQUEST
 
 
 class AccountOwnerIsMinor(HTTPException):
     def __init__(self) -> None:
-        """Raised when the account owner is minor."""
-        self.detail = 'Você precisa ser maior de idade para criar uma conta.'
+        """Raised when the account owner is underage."""
+        self.detail = 'You must be of legal age to create an account.'
         self.status_code = status.HTTP_400_BAD_REQUEST
 
 
 class CantUpdateAccount(HTTPException):
     def __init__(self) -> None:
-        """Raised when the current user is trying to update other user account."""
-        self.detail = 'Você não tem permissão para editar essa conta.'
+        """Raised when the current user is trying to update another user account."""
+        self.detail = 'You do not have permission to edit this account.'
         self.status_code = status.HTTP_403_FORBIDDEN
 
 
 class CantUpdateDailyWithdrawLimit(HTTPException):
     def __init__(self) -> None:
-        """Raised when a account owner tries to update the daily withdraw limit to less then he already withdraw today"""
-        self.detail = 'Você não pode alterar o limite de saque diário para um menor do que já foi sacado hoje.'
+        """Raised when an account owner tries to update the daily withdrawal limit 
+        to less than what has already been withdrawn today."""
+        self.detail = 'You cannot change the daily withdrawal limit to a value lower than what has already been withdrawn today.'
         self.status_code = status.HTTP_400_BAD_REQUEST
 
 
 class CantBlockAccount(HTTPException):
     def __init__(self) -> None:
-        """Raised when the current user is trying to block other user account."""
-        self.detail = 'Você não tem permissão para bloquear essa conta.'
+        """Raised when the current user is trying to block another user account."""
+        self.detail = 'You do not have permission to block this account.'
         self.status_code = status.HTTP_403_FORBIDDEN
